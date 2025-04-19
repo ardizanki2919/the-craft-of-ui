63db8b1c2ecba713e53207f9ade57c24b016c36a
jh3y <jh3y@users.noreply.github.com>
2025-04-19 11:42:42 +0100

    feat: api foundation for form

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,17 @@ import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
